7c404e5e40d8110bd804e105afa95f885115b336
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-12-13 20:02:50 -0300

    feat: add img na page perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import logo from '../img/logo.png';
 
 function Perfil() {
   return (
-    <div>
-      <h1 className="perfil">Perfil Netflix</h1>
-    </div>
+    <>
+      <div>
+        <img src={logo} width="150" alt="logo netflix" />
+      </div>
+      <main>
+        <div>
+          <h1>Quem está assistindo?</h1>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </main>
+      <boton className="botton-perfil">GERENCIAR PERFIS</boton>
+    </>
   );
 }
 
