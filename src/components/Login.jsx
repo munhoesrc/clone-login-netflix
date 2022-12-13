c53526bad6ed5f44e34f120b3a7f1adc5274c6c1
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-12-13 16:03:24 -0300

    feat: resolvendo conflito

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import logo from '../img/logo.png';
 
 function Login() {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('mudar de rota');
+      history.push('/perfil');
+    }, 5000);
+  }, []);
+
   return (
     <>
       <div className="logo-netflix">
