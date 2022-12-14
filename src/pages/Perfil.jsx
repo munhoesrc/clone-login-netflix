580d7334854694766221a6c6c54d748e79f37b50
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-12-13 21:30:19 -0300

    feat: perfil com imagens e nomes

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import logo from '../img/logo.png';
 
+import imagemFirst from '../img/imgperfil1.png';
+import imagemSecond from '../img/imgperfil2.jpg';
+import imagemFour from '../img/imgperfilkids.png';
+
 function Perfil() {
   return (
     <>
-      <div>
+      <div className="img-pqn">
         <img src={logo} width="150" alt="logo netflix" />
       </div>
-      <main>
+      <div>
+        <h1>Quem está assistindo?</h1>
+      </div>
+      <main className="all-img">
         <div>
-          <h1>Quem está assistindo?</h1>
+          <img src={imagemFirst} width="150" alt="avatar perfil" />
+          <span>Pedro</span>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <img src={imagemSecond} width="150" alt="avatar perfil" />
+          <span>Sue Storm</span>
+        </div>
+        <div>
+          <img src={imagemFour} width="150" alt="avatar perfil" />
+          <span>Donald McConnor</span>
+        </div>
+        <div>
+          <span>Adicionar perfil</span>
+        </div>
       </main>
       <boton className="botton-perfil">GERENCIAR PERFIS</boton>
     </>
